0b99ac839f5de263230a4989f8a4f8bee817c527
Victor Korelsky <victor.korelsky@mail.mcgill.ca>
2017-11-26 20:42:14 -0500

    bumped up ruby version

--- bot.rb
+++ bot.rb
@@ -1,1 +1,12 @@
 require "dotenv/load"
+require "bittrex"
+
+Bittrex.config do |c|
+  c.key = ENV['API_KEY']
+  c.secret = ENV['API_SECRET']
+end
+
+Bittrex::Summary.all.each do |market|
+  puts market
+  puts ""
+end
